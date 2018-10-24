279cc4d804dfbf12e5b687b6510592e385e211b7
Jayakrishnan G <jkdesignlab@gmail.com>
2018-10-24 12:26:03 +0530

    Seed and Auction  arena changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 Role.find_or_create_by(name: 'admin')
-Role.find_or_create_by(name: 'user')
+Role.find_or_create_by(name: 'player')
 Role.find_or_create_by(name: 'team_owner')
+
+admin_user = User.find_or_create_by(name:'Admin', email:'admin@auctionmanager.com', password:'admin123', password_confirmation:'admin123')
+UsersRole.find_or_create_by(role_id: 1, user_id: admin_user.id)
 
 seed_file = Rails.root.join('db', 'seeds', 'users.yml')
 users = YAML::load_file(seed_file)
 users.each do |user|
   user['email'] = "#{user['name']}@rubyians.com".gsub(' ', '').downcase
-  u = User.create(user)
-  UsersRole.create(role_id: 2, user_id: u.id)
+  u = User.find_or_create_by(user)
+  UsersRole.find_or_create_by(role_id: 2, user_id: u.id)
 end
